1b28a114c84d1c69ed19b36de83643c25f9bb4e1
Rick Ball <41898282+rickballard@users.noreply.github.com>
2025-10-03 01:38:46 -0400

    telemetry: contact intent beacon

--- assets/site.js
+++ assets/site.js
@@ -1,3 +1,8 @@
 (function(){try{
   new Image().src="/__t?ev=page&p="+encodeURIComponent(location.pathname);
 }catch(e){}})();
+document.addEventListener("click", e=>{
+  const a=e.target.closest('a[href^="mailto:"],a[href*="/contact"]');
+  if(!a) return;
+  try{ new Image().src="/__t?ev=contact&p="+encodeURIComponent(location.pathname); }catch(e){}
+}, {capture:true});
